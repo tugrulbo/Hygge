f7351b5d6a151411acf3ff68fb27b6ec4b3d4f42
tugrulb <tugrulbo@gmail.com>
2021-06-17 01:40:53 +0300

    Initial commit

---
+++ app/src/main/java/com/tugrulbo/hygge/ui/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.tugrulbo.hygge.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.tugrulbo.hygge.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
---
+++ app/src/main/java/com/tugrulbo/hygge/ui/home/HomeViewModel.kt
@@ -0,0 +1,7 @@
+package com.tugrulbo.hygge.ui.home
+
+import androidx.lifecycle.ViewModel
+
+class HomeViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
